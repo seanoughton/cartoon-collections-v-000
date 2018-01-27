941def1e2793e6f2ea175190596043c62aa49116
Sean Oughton <seanoughton@gmail.com>
2018-01-27 00:32:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)
-  
+  array.collect do|string|
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
